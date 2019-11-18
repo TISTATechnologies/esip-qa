32d19f90ec9e76ec1e341184d6c093998b37dcce
g.gardner <ggardner@tistatech.com>
2019-11-18 11:05:12 -0500

    Consolidated tests, added comments

--- src/test/java/demo/EsipDemoQA.java
+++ src/test/java/demo/EsipDemoQA.java
@@ -16,7 +16,7 @@ import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
-public class EsipDemoQAHapi {
+public class EsipDemoQA {
     private static String log4j2ConfigFile = "./Properties/log4j2.xml";
     private static String API_SOURCE_SERVER_HOST = "http://52.202.77.60:9090/esip/v1/";
     private static String API_TARGET_SERVER_HOST = "http://3.133.161.132:9090/esip/v1/";
@@ -31,12 +31,13 @@ public class EsipDemoQAHapi {
     static String dateOfBirth = "";
 
     @Test
-    public void test1createPatient() {
+    public void test1createPatientHapiFHIR() {
         System.setProperty("log4j.configurationFile", log4j2ConfigFile);
         Logger logger = LogManager.getLogger(this.getClass());
         Faker faker = new Faker();
         Random random = new Random();
         long randomDay = (int) LocalDate.of(1900, 1, 1).toEpochDay() + random.nextInt((int) LocalDate.of(2019, 1, 1).toEpochDay() - (int) LocalDate.of(1900, 1, 1).toEpochDay());
+        System.out.println("RUNNING THE HAPI API TESTS");
         firstName = faker.name().firstName();
         lastName = faker.name().lastName();
         middleName = faker.name().firstName();
@@ -52,6 +53,12 @@ public class EsipDemoQAHapi {
                 .replace("@@dateOfBirth@@", dateOfBirth).replace("@@email@@", email);
         logger.info("Attempting to connect to Hapi FHIR server and create a new Patient where Request body is: ");
         System.out.println(requestString );
+
+        /*
+         *
+         * SERVER 1 POST REQUEST - POSTING A NEW PATIENT RECORD THROUGH HAPI API.
+         *
+         * */
         Response response = given().header(header)
                 .and().body(requestString)
                 .when().post(ENDPOINT_SOURCE_CREATE_PATIENT);
@@ -61,6 +68,11 @@ public class EsipDemoQAHapi {
         logger.info("New Patient has been successfully created with id: " + patientID);
         System.out.println("");
         logger.info("Attempting to connect to database #1 and find newly created Patient by it's id: " + patientID);
+        /*
+         *
+         * SERVER 2 Verification SHOWING REPLICATION HAPI API
+         *
+         * */
         response = given().header(header)
                 .when().get(ENDPOINT_SOURCE_GET_PATIENT + patientID);
         assertEquals(200, response.statusCode());
@@ -75,12 +87,98 @@ public class EsipDemoQAHapi {
         logger.warn("Last Name is equal to: " + jPath.getString("lastName"));
         logger.warn("Date Of Birth is equal to: " + jPath.getString("dateOfBirth"));
         System.out.println("");
+        /*
+         *
+         * SERVER 2 Verification SHOWING REPLICATION
+         *
+         * */
         logger.info("Attempting to connect to database #2 and find newly created Patient by it's id: " + patientID);
         response = given().header(header)
                .when().get(ENDPOINT_TARGET_GET_PATIENT + patientID);
         assertEquals(200, response.statusCode());
         logger.info("Successfully connected to Server | Status code is: 200");
-        logger.info("Patien with id: " + patientID + " has below deatails:");
+        logger.info("Patient with id: " + patientID + " has below deatails:");
+        System.out.println("");
+        System.out.println(response.asString());
+        System.out.println("");
+        jPath = new JsonPath(response.asString());
+        logger.warn("First Name is equal to: " + jPath.getString("firstName"));
+        logger.warn("Middle Name is equal to: " + jPath.getString("middleName"));
+        logger.warn("Last Name is equal to: " + jPath.getString("lastName"));
+        logger.warn("Date Of Birth is equal to: " + jPath.getString("dateOfBirth"));
+    }
+
+    @Test
+    public void test2createPatientMicroservices() {
+        System.setProperty("log4j.configurationFile", log4j2ConfigFile);
+        Logger logger = LogManager.getLogger(this.getClass());
+        Faker faker = new Faker();
+        Random random = new Random();
+        long randomDay = (int) LocalDate.of(1900, 1, 1).toEpochDay() + random.nextInt((int) LocalDate.of(2019, 1, 1).toEpochDay() - (int) LocalDate.of(1900, 1, 1).toEpochDay());
+        System.out.println("RUNNING THE MICROSERVICE TESTS");
+        firstName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        middleName = faker.name().firstName();
+        dateOfBirth = LocalDate.ofEpochDay(randomDay).toString();
+        logger.info("Generated First Name is: " + firstName);
+        logger.info("Generated Last Name is: " + lastName);
+        logger.info("Generated Middle Name is: " + middleName);
+        logger.info("Generated Date Of Birth is: " + dateOfBirth);
+        String email = firstName + lastName + "@mail.com";
+        logger.info("Generated Email address is: " + email);
+        String requestString = convertRequestToString("./src/test/java/jsonFiles/create_patient.json")
+                .replace("@@FirstName@@", firstName).replace("@@LastName@@", lastName).replace("@@MiddleName@@", middleName)
+                .replace("@@dateOfBirth@@", dateOfBirth).replace("@@email@@", email);
+        logger.info("Attempting to connect to Miscroservices server and create a new Patient where Request body is: ");
+        System.out.println(requestString );
+        /*
+         *
+         * SERVER 1 POST REQUEST - POSTING A REQUEST RECORD.
+         *
+         * */
+
+
+        Response response = given().header(header)
+                .and().body(requestString)
+                .when().post(ENDPOINT_SOURCE_CREATE_PATIENT);
+        assertEquals(200, response.statusCode());
+        logger.info("Successfully connected to Microservice Server | Status code is: 200");
+        patientID = response.asString().split(" - ")[1];
+        logger.info("New Patient has been successfully created with id: " + patientID);
+        System.out.println("");
+
+        /*
+         *
+         * SERVER 1 GET REQUEST - RETRIEVING THE POST REQUEST RECORD.
+         *
+         * */
+        logger.info("Attempting to connect to database #1 and find newly created Patient by it's id: " + patientID);
+        response = given().header(header)
+                .when().get(ENDPOINT_SOURCE_GET_PATIENT + patientID);
+        assertEquals(200, response.statusCode());
+        logger.info("Successfully connected to Server | Status code is: 200");
+        logger.info("Patient with id: " + patientID + " has below details:");
+        System.out.println("");
+        System.out.println(response.asString());
+        System.out.println("");
+        JsonPath jPath = new JsonPath(response.asString());
+        logger.warn("First Name is equal to: " + jPath.getString("firstName"));
+        logger.warn("Middle Name is equal to: " + jPath.getString("middleName"));
+        logger.warn("Last Name is equal to: " + jPath.getString("lastName"));
+        logger.warn("Date Of Birth is equal to: " + jPath.getString("dateOfBirth"));
+        System.out.println("");
+        logger.info("Attempting to connect to database #2 and find newly created Patient by it's id: " + patientID);
+
+        /*
+         *
+         * SERVER 2 Verification SHOWING REPLICATION
+         *
+         * */
+        response = given().header(header)
+                .when().get(ENDPOINT_TARGET_GET_PATIENT + patientID);
+        assertEquals(200, response.statusCode());
+        logger.info("Successfully connected to Server | Status code is: 200");
+        logger.info("Patient with id: " + patientID + " has below deatails:");
         System.out.println("");
         System.out.println(response.asString());
         System.out.println("");
